d680dbf4a899d874bbafd3a1ddd290870111b062
Curtis Vanzandt <ctvanzandt42@gmail.com>
2020-04-26 10:38:00 -0500

    Added views for participants, home page, and participant details

--- family_member_data/urls.py
+++ family_member_data/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('participants', views.participants, name='participants'),
+    path('participants/<int:participant_id>/', views.participant_details, name='participant_details')
 ]
--- family_member_data/views.py
+++ family_member_data/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Participant
 
 
 def index(request):
-    return HttpResponse("Welcome to the area to post gift preferences")
+    return render(request, 'family_member_data/index.html')
+
+
+def participants(request):
+    participant_list = Participant.objects.all()
+    context = {
+        'participant_list': participant_list,
+    }
+    return render(request, 'family_member_data/participants.html', context)
+
+
+def participant_details(request, participant_id):
+    participant = get_object_or_404(Participant, pk=participant_id)
+    context = {
+        'participant': participant,
+        'participant_id': participant_id,
+    }
+    return render(request, 'family_member_data/participant_detail.html', context)
