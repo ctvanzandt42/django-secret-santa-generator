c1a273a8f57fe3361e14fa3fbcb4c590879ee262
Curtis Vanzandt <ctvanzandt42@gmail.com>
2020-04-25 21:47:33 -0500

    Added Participant model and made updates to admin
    I created an admin superuser to keep track of partipants. I've also created the Participant model, and after some struggle, figured out how to make certain fields optional.

--- family_member_data/models.py
+++ family_member_data/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Participant(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(default=None, blank=True, null=True)
+    email = models.CharField(max_length=30)
+    requests = models.CharField(max_length=500, blank=True, null=True)
+    assignee = models.IntegerField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
